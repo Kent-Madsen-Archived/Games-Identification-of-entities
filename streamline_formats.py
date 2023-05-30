65ce4c48862fc2683ea1b38988d7f6afa55fe821
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-05-30 13:49:07 +0200

    adding python

---
+++ streamline_formats.py
@@ -0,0 +1,18 @@
+from os.path    \
+    import      \
+        join,   \
+        isdir,  \
+        isfile
+
+from os     \
+    import  \
+    walk,   \
+    rename
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
